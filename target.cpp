c92f7912409ed997404fd4641153d2c13b0b6c28
Pythonmcpi <47196755+pythonmcpi@users.noreply.github.com>
2021-06-21 17:51:16 +0800

    Update target.cpp with build instructions, add built file to gitignore

--- target.cpp
+++ target.cpp
@@ -1,3 +1,14 @@
+/*
+ * Compile using:
+ *
+ *   g++ target.cpp -o target.exe
+ *
+ * Tested on windows 8.1 when using mingw64,
+ * although windows 10 will probably also work.
+ * Compiling on linux is untested but should work.
+ *
+ */
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,4 +28,3 @@ int main(int argc, char**argv){
 	}
 	return 0;
 }
-
